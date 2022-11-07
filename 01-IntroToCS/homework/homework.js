16cf0d80611059f0407ac925645432a29bd00a2c
becaale <becaale@hotmail.com>
2022-11-07 18:04:09 +0200

    Homework CS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let numDecimal = 0;
+  let numBinario = num.split("").reverse().join("");
 
+  for (let i = 0; i < numBinario.length; i++){
+    numDecimal += numBinario[i] * 2 ** i;
+  } 
+
+  return numDecimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let numDecimal = num;
+  let numBinario = [];
 
+  while (numDecimal>0){
+    numBinario.push(numDecimal%2)
+    numDecimal = Math.floor(numDecimal / 2)
+  }
+ return numBinario.reverse().join("");
 }
 
 
