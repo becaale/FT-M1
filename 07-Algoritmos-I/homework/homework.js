f45288576dfba6c857d1a2b064e2efa0ff7fd651
becaale <becaale@hotmail.com>
2022-11-15 17:25:21 +0200

    Homework Algoritmos-I factoreo ok

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,19 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  const arrPrimos = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101];
+  let arrFact = [1];
+  let i = 0;
+
+  while (i < arrPrimos.length) {
+    if(num%arrPrimos[i] === 0){
+      arrFact.push(arrPrimos[i]);
+      num = num / arrPrimos[i]
+    } else {
+      i++
+    }    
+  }
+  return arrFact;
 }
 
 function bubbleSort(array) {
@@ -14,6 +27,14 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      const element = array[j];
+      
+    }
+    const element = array[i];
+    
+  }
 
 }
 
